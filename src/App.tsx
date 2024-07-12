d0d5513ee725d2108feed04fb9ccc658928d8b8f
Taisei <taisei.960420@gmail.com>
2024-07-12 20:01:33 +0900

    コメント復活

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   const handleScroll = () => {
     const scrollTop = window.scrollY;
-    if (scrollTop > 50) { // 50px以上スクロールしたら表示
+    if (scrollTop > 50) { // 70px以上スクロールしたら表示
       setShowMenu(true);
     } else {
       setShowMenu(false)
