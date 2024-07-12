de10050e5858c302bb5dc519343c77a9c3219ddc
Taisei <taisei96.web@gmail.com>
2024-07-12 19:54:42 +0900

    変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Menu from './Menu';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    if (scrollTop > 50) { // 100px以上スクロールしたら表示
+      setShowMenu(true);
+    } else {
+      setShowMenu(false)
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-4xl font-bold text-black">Scroll Menu</h1>
       </header>
+      <Menu show={showMenu} />
     </div>
   );
 }
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./Menu.css";
 
-const Menu = () => {
+interface MenuProps {
+  show: boolean;
+}
+
+const Menu: React.FC<MenuProps> = ({show}) => {
   return (
-    <div>Menu</div>
-  )
+    <ul className={`menu bg-base-200 rounded-box w-56 ${show ? 'show' : ''}`}>
+      <li>
+        <h2 className="menu-title">Title</h2>
+        <ul>
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </li>
+    </ul>
+  );
 }
 
 export default Menu
